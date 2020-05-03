59b8b692dabc04b3814ad56e130722745c351afe
KappaKeppo <alexstrelnikof@gmail.com>
2020-05-03 13:30:09 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+import './app_screens/first_screen.dart';
+
+
+void main() => runApp(new MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: "My Flutter App",
+        home: Scaffold(
+            appBar: AppBar(title: Text("My First App Screen"),),
+            body: FirstScreen()
+        )
+    );
+  }
+}
